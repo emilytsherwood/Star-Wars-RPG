a3c383400a5221882c4114d45be8edaede2535bf
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2016-10-26 11:35:16 -0500

    appending elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,37 +22,67 @@ var princessLeia = {
 	counterAttackPower: 11,
 }
 
+
+
 //When a character is chosen, the other 3 get put into enemies
 //Is there a for loop/function to do this more compactly?
 $(document).ready(function(){
-});
 
-function characterChoice(){
-	$('.darth').click(function(){
-		$('.yoda').appendTo('.enemiesSection')
-		$('.leia').appendTo('.enemiesSection')
-		$('.kylo').appendTo('.enemiesSection')
+//Creating a variable for each section
+var choice = $('.enemiesSection');
+var defender = $('.defenderSection');
+
+function darthChoice(){
+	$('.darth').one('click',function(){
+		$('.yoda').appendTo(choice);
+		$('.leia').appendTo(choice);
+		$('.kylo').appendTo(choice);
 	})
+};
 
-	$('.yoda').click(function(){
-		$('.darth').appendTo('.enemiesSection')
-		$('.leia').appendTo('.enemiesSection')
-		$('.kylo').appendTo('.enemiesSection')
-	});
+function yodaChoice(){
+	$('.yoda').one('click',function(){
+		$('.darth').appendTo(choice);
+		$('.leia').appendTo(choice);
+		$('.kylo').appendTo(choice);
+	})
+};
 
-	$('.kylo').click(function(){
-		$('.darth').appendTo('.enemiesSection')
-		$('.leia').appendTo('.enemiesSection')
-		$('.yoda').appendTo('.enemiesSection')
-	});
+function kyloChoice(){
+	$('.kylo').one('click',function(){
+		$('.darth').appendTo(choice);
+		$('.leia').appendTo(choice);
+		$('.yoda').appendTo(choice);
+	})
+};
 
-	$('.leia').click(function(){
-		$('.darth').appendTo('.enemiesSection')
-		$('.kylo').appendTo('.enemiesSection')
-		$('.yoda').appendTo('.enemiesSection')
+function leiaChoice(){
+	$('.leia').one('click',function(){
+		$('.darth').appendTo(choice);
+		$('.kylo').appendTo(choice);
+		$('.yoda').appendTo(choice);
 	});
 };
-characterChoice();
+
+//If any of these are clicked, all the other functions need to stop
+
+darthChoice();
+yodaChoice();
+kyloChoice();
+leiaChoice();
+
+//On 2nd click, the characters clicked on moves to defender section
+
+function darthDefender() {
+	$('.darth').one('click',function(){
+		$('darth').appendTo(defender);
+	});
+}
+
+
+});//End of doc.ready function at top
+
+
 
 
 
